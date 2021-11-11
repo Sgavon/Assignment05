2517242178cd4612098ff00a674a601bf35f929d
Sgavon <94145071+Sgavon@users.noreply.github.com>
2021-11-11 13:48:36 -0800

    Add files via upload

---
+++ CDInventory_Dictionary.py
@@ -0,0 +1,71 @@
+#------------------------------------------#
+# Title: CDInventory.py
+# Desc: Starter Script for Assignment 05
+# Change Log: (Who, When, What)
+# DBiesinger, 2030-Jan-01, Created File
+#------------------------------------------#
+
+# Declare variabls
+
+strChoice = '' # User input
+lstTbl = []  # list of lists to hold data
+# TODO replace list of lists with list of dicts
+dicRow = {}  # dictionary of data row
+strFileName = 'CDInventory.txt'  # data storage file
+objFile = None  # file object
+
+# Get user Input
+print('The Magic CD Inventory\n')
+while True:
+    # 1. Display menu allowing the user to choose:
+    print('[l] load Inventory from file\n[a] Add CD\n[i] Display Current Inventory')
+    print('[d] delete CD from Inventory\n[s] Save Inventory to file\n[x] exit')
+    strChoice = input('l, a, i, d, s or x: ').lower()  # convert choice to lower case at time of input
+    print()
+
+    if strChoice == 'x':
+        # 5. Exit the program if the user chooses so
+        break
+    if strChoice == 'l':
+        # TODO Add the functionality of loading existing data
+        objFile = open(strFileName, 'r')
+        for row in objFile:
+            lstRow = row.strip().split(',')
+            dicRow = {'ID':lstRow[0], 'Title':lstRow[1], 'Artist':lstRow[2]}
+            lstTbl.append(dicRow)
+        objFile.close()
+        
+    elif strChoice == 'a':  # no elif necessary, as this code is only reached if strChoice is not 'exit'
+        # 2. Add data to the table (2d-list) each time the user wants to add data
+        strID = input('Enter an ID: ')
+        strTitle = input('Enter the CD\'s Title: ')
+        strArtist = input('Enter the Artist\'s Name: ')
+        intID = int(strID)
+        dicRow = {'ID':strID, 'Title':strTitle, 'Artist':strArtist}
+        lstTbl.append(dicRow)
+    elif strChoice == 'i':
+        # 3. Display the current data to the user each time the user wants to display the data
+        print('ID, CD Title, Artist')
+        for row in lstTbl:
+            print(row['ID'], row['Title'], row['Artist'],sep = ', ')
+    elif strChoice == 'd':
+        # TODO Add functionality of deleting an entry
+        DeleteID = input('Input the ID to delete: ')
+        for row in lstTbl:
+            if row['ID'] == DeleteID:
+                lstTbl.remove(row)
+            else:
+                continue
+    elif strChoice == 's':
+        # 4. Save the data to a text file CDInventory.txt if the user chooses so
+        objFile = open(strFileName, 'a')
+        for row in lstTbl:
+            strName = ''
+            for key, val in row.items():
+                strName += str(val) + ','
+            strName = strName[:-1] + '\n'
+            objFile.write(strName)
+        objFile.close()
+    else:
+        print('Please choose either l, a, i, d, s or x!')
+
